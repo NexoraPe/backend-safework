aa893671b822bf30b2a425c7a2d045fbbe1e4f86
AmirbarrabajaLeon <amircasan@gmail.com>
2025-11-04 18:08:41 -0500

    chore: Enable JPA auditing in BackSafeworkApplication

--- src/main/java/com/nexorape/safework/service/BackSafeworkApplication.java
+++ src/main/java/com/nexorape/safework/service/BackSafeworkApplication.java
@@ -2,12 +2,13 @@ package com.nexorape.safework.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class BackSafeworkApplication {
+@EnableJpaAuditing
 
+public class BackSafeworkApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackSafeworkApplication.class, args);
     }
-
 }
