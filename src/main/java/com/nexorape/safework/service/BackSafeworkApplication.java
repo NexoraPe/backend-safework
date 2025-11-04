8ba86d29ff560769281095f928f7213d6a6d3e1f
AmirbarrabajaLeon <amircasan@gmail.com>
2025-11-04 17:36:38 -0500

    chore: initial commit.

---
+++ src/main/java/com/nexorape/safework/service/BackSafeworkApplication.java
@@ -0,0 +1,13 @@
+package com.nexorape.safework.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSafeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackSafeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nexorape/safework/service/BackSafeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexorape.safework.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSafeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
