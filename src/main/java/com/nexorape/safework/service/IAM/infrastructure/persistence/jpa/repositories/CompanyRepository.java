a7f73d159aa69cc503d139de5bc7cee7f1abfd81
AmirbarrabajaLeon <amircasan@gmail.com>
2025-11-05 16:19:31 -0500

    feat: add Company and User repositories for IAM domain persistence

---
+++ src/main/java/com/nexorape/safework/service/IAM/infrastructure/persistence/jpa/repositories/CompanyRepository.java
@@ -0,0 +1,13 @@
+package com.nexorape.safework.service.IAM.infrastructure.persistence.jpa.repositories;
+
+import com.nexorape.safework.service.IAM.domain.model.aggregates.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Optional<Company> findByRegistrationCode(String registrationCode);
+
+}
