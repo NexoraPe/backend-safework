fd9b92be6c78327898ef231de7b48f3decf397fe
AmirbarrabajaLeon <amircasan@gmail.com>
2025-11-06 00:35:34 -0500

    feat: update commands to include companyId for company name and registration code updates

--- src/main/java/com/nexorape/safework/service/IAM/domain/model/commands/UpdateCompanyNameCommand.java
+++ src/main/java/com/nexorape/safework/service/IAM/domain/model/commands/UpdateCompanyNameCommand.java
@@ -1,7 +1,12 @@
 package com.nexorape.safework.service.IAM.domain.model.commands;
 
-public record UpdateCompanyNameCommand(String newName) {
+public record UpdateCompanyNameCommand(Long companyId, String newName) {
     public UpdateCompanyNameCommand{
+
+        if(companyId == null || companyId <= 0){
+            throw new IllegalArgumentException("companyId is null or empty");
+        }
+
         if (newName == null || newName.isBlank()) {
             throw new IllegalArgumentException("New name cannot be null or blank");
         }
--- src/main/java/com/nexorape/safework/service/IAM/domain/model/commands/UpdateCompanyRegistrationCodeCommand.java
+++ src/main/java/com/nexorape/safework/service/IAM/domain/model/commands/UpdateCompanyRegistrationCodeCommand.java
@@ -1,7 +1,12 @@
 package com.nexorape.safework.service.IAM.domain.model.commands;
 
-public record UpdateCompanyRegistrationCodeCommand(String newCompanyRegistrationCode) {
+public record UpdateCompanyRegistrationCodeCommand(Long companyId, String newCompanyRegistrationCode) {
     public UpdateCompanyRegistrationCodeCommand {
+
+        if(companyId == null || companyId <= 0) {
+            throw new IllegalArgumentException("companyId is null or empty");
+        }
+
         if (newCompanyRegistrationCode == null || newCompanyRegistrationCode.isBlank()) {
             throw new IllegalArgumentException("New Company Registration Code cannot be null or blank");
         }
