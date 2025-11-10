14d4c6bfe17cf819d5cc775be2f1936c4fa6d0c2
AmirbarrabajaLeon <amircasan@gmail.com>
2025-11-10 00:38:19 -0500

    feat: delete old user components

--- src/main/java/com/nexorape/safework/service/IAM/domain/model/commands/UpdateUserPersonalInfoCommand.java
+++
@@ -1,9 +0,0 @@
-package com.nexorape.safework.service.IAM.domain.model.commands;
-
-public record UpdateUserPersonalInfoCommand(String newFullName, String newPhoneNumber) {
-    public UpdateUserPersonalInfoCommand {
-        if (newFullName == null || newFullName.isEmpty()) {
-            throw new IllegalArgumentException("Full name cannot be null or empty");
-        }
-    }
-}
--- src/main/java/com/nexorape/safework/service/IAM/domain/model/queries/GetUserByEmailQuery.java
+++
@@ -1,9 +0,0 @@
-package com.nexorape.safework.service.IAM.domain.model.queries;
-
-public record GetUserByEmailQuery(String email) {
-    public GetUserByEmailQuery {
-        if (email == null || email.isBlank()) {
-            throw new IllegalArgumentException("Email is null or empty");
-        }
-    }
-}
