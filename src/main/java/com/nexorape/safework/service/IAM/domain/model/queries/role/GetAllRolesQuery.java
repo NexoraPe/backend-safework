80a2a48f072144501ea88f802c0fa5bb5b5e1f4a
AmirbarrabajaLeon <amircasan@gmail.com>
2025-11-10 02:15:58 -0500

    feat: add queries for retrieving roles and refactor SeedRolesCommand package structure

---
+++ src/main/java/com/nexorape/safework/service/IAM/domain/model/queries/role/GetAllRolesQuery.java
@@ -0,0 +1,4 @@
+package com.nexorape.safework.service.IAM.domain.model.queries.role;
+
+public record GetAllRolesQuery() {
+}
---
+++ src/main/java/com/nexorape/safework/service/IAM/domain/model/queries/role/GetRoleByNameQuery.java
@@ -0,0 +1,6 @@
+package com.nexorape.safework.service.IAM.domain.model.queries.role;
+
+import com.nexorape.safework.service.IAM.domain.model.valueobjects.Roles;
+
+public record GetRoleByNameQuery(Roles name) {
+}
