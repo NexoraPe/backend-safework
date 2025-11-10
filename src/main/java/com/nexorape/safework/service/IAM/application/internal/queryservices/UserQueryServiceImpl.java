085ede016c9bc90179ac60b82531b7b05d19dd92
AmirbarrabajaLeon <amircasan@gmail.com>
2025-11-10 03:58:30 -0500

    feat: implement UserCommandService and UserQueryService for user management

---
+++ src/main/java/com/nexorape/safework/service/IAM/application/internal/queryservices/UserQueryServiceImpl.java
@@ -0,0 +1,62 @@
+package com.nexorape.safework.service.IAM.application.internal.queryservices;
+
+import com.nexorape.safework.service.IAM.domain.model.aggregates.User;
+import com.nexorape.safework.service.IAM.domain.model.queries.user.GetAllUsersQuery;
+import com.nexorape.safework.service.IAM.domain.model.queries.user.GetUserByEmailQuery;
+import com.nexorape.safework.service.IAM.domain.model.queries.user.GetUserByIdQuery;
+import com.nexorape.safework.service.IAM.domain.services.user.UserQueryService;
+import com.nexorape.safework.service.IAM.infrastructure.persistence.jpa.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Implementation of {@link UserQueryService} interface.
+ */
+@Service
+public class UserQueryServiceImpl implements UserQueryService {
+    private final UserRepository userRepository;
+
+    /**
+     * Constructor.
+     *
+     * @param userRepository {@link UserRepository} instance.
+     */
+    public UserQueryServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * This method is used to handle {@link GetAllUsersQuery} query.
+     * @param query {@link GetAllUsersQuery} instance.
+     * @return {@link List} of {@link User} instances.
+     * @see GetAllUsersQuery
+     */
+    @Override
+    public List<User> handle(GetAllUsersQuery query) {
+        return userRepository.findAll();
+    }
+
+    /**
+     * This method is used to handle {@link GetUserByIdQuery} query.
+     * @param query {@link GetUserByIdQuery} instance.
+     * @return {@link Optional} of {@link User} instance.
+     * @see GetUserByIdQuery
+     */
+    @Override
+    public Optional<User> handle(GetUserByIdQuery query) {
+        return userRepository.findById(query.userId());
+    }
+
+    /**
+     * This method is used to handle {@link GetUserByEmailQuery} query.
+     * @param query {@link GetUserByEmailQuery} instance.
+     * @return {@link Optional} of {@link User} instance.
+     * @see GetUserByEmailQuery
+     */
+    @Override
+    public Optional<User> handle(GetUserByEmailQuery query) {
+        return userRepository.findByEmail(query.email());
+    }
+}
