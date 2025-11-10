d16c1fd3ba003374fdc69c51936ecfe8fa964ee4
AmirbarrabajaLeon <amircasan@gmail.com>
2025-11-09 21:07:42 -0500

    chore: Deleted old files

--- src/main/java/com/nexorape/safework/service/IAM/domain/model/queries/ExistsByRegistrationCodeQuery.java
+++
@@ -1,4 +0,0 @@
-package com.nexorape.safework.service.IAM.domain.model.queries;
-
-public record ExistsByRegistrationCodeQuery(String registrationCode) {
-}
--- src/main/java/com/nexorape/safework/service/IAM/interfaces/resources/CompanyResource.java
+++
@@ -1,4 +0,0 @@
-package com.nexorape.safework.service.IAM.interfaces.resources;
-
-public record CompanyResource(Long id, String name, String registrationCode) {
-}
