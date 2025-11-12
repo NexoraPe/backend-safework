838907087c1bcdabf1d3d27979e009bf5f6960dc
AmirbarrabajaLeon <amircasan@gmail.com>
2025-11-12 16:02:06 -0500

    feat: implement AssignmentQueryServiceImpl and update IncidentCommandService for assignment handling

--- src/main/java/com/nexorape/safework/service/incidentmanagement/application/internal/commandservices/IncidentCommandServiceImpl.java
+++ src/main/java/com/nexorape/safework/service/incidentmanagement/application/internal/commandservices/IncidentCommandServiceImpl.java
@@ -3,7 +3,9 @@ package com.nexorape.safework.service.incidentmanagement.application.internal.co
 import com.nexorape.safework.service.iam.infrastructure.persistence.jpa.repositories.CompanyRepository;
 import com.nexorape.safework.service.iam.infrastructure.persistence.jpa.repositories.UserRepository;
 import com.nexorape.safework.service.incidentmanagement.domain.model.aggregates.Incident;
-import com.nexorape.safework.service.incidentmanagement.domain.model.commands.CreateIncidentCommand;
+import com.nexorape.safework.service.incidentmanagement.domain.model.commands.assignment.CreateAssignmentCommand;
+import com.nexorape.safework.service.incidentmanagement.domain.model.commands.incident.CreateIncidentCommand;
+import com.nexorape.safework.service.incidentmanagement.domain.model.entities.Assignment;
 import com.nexorape.safework.service.incidentmanagement.domain.services.IncidentCommandService;
 import com.nexorape.safework.service.incidentmanagement.infrastructure.persistence.jpa.repositories.IncidentRepository;
 import org.springframework.stereotype.Service;
@@ -29,12 +31,29 @@ public class IncidentCommandServiceImpl implements IncidentCommandService {
 
         // SOLUCION RAPIDA, MAS ADELANTE YA SEGUIMOS DDD AL 100% XD
 
-        var user = userRepository.findById(command.userId()).orElseThrow(() -> new RuntimeException("User id not found"));;
-        var company = companyRepository.findById(command.companyId()).orElseThrow(() -> new RuntimeException("Company id not found"));;
+        var user = userRepository.findById(command.userId()).orElseThrow(() -> new RuntimeException("User not found"));;
+        var company = companyRepository.findById(command.companyId()).orElseThrow(() -> new RuntimeException("Company not found"));;
 
         var incident = new Incident(user, company, command);
         incidentRepository.save(incident);
         return Optional.of(incident);
     }
 
+
+    //ASSIGMENTS
+    @Override
+    public Optional<Assignment> handle(CreateAssignmentCommand command){
+        var incident = incidentRepository.findById(command.incidentId()).orElseThrow(() -> new RuntimeException("Incident not found"));
+        var user = userRepository.findById(command.userId()).orElseThrow(() -> new RuntimeException("User not found"));
+
+        incident.assignTo(user);
+
+        //NO LLAMAR AL REPOSITORIO DE ASSIGMENT, EL INCIDENT YA SE ENCARGA DE LA CREACION EN LA BASE DE DATOS
+        // SOLO ESTA PARA LECTURA EN EL QUERYSERVICES
+
+        incidentRepository.save(incident);
+
+
+        return Optional.of(incident.getAssignment());
+    }
 }
---
+++ src/main/java/com/nexorape/safework/service/incidentmanagement/application/internal/queryservices/AssignmentQueryServiceImpl.java
@@ -0,0 +1,32 @@
+package com.nexorape.safework.service.incidentmanagement.application.internal.queryservices;
+
+import com.nexorape.safework.service.incidentmanagement.domain.model.entities.Assignment;
+import com.nexorape.safework.service.incidentmanagement.domain.model.queries.assignment.GetAllAssignmentsQuery;
+import com.nexorape.safework.service.incidentmanagement.domain.model.queries.assignment.GetAssignmentByIdQuery;
+import com.nexorape.safework.service.incidentmanagement.domain.services.AssignmentQueryService;
+import com.nexorape.safework.service.incidentmanagement.infrastructure.persistence.jpa.repositories.AssignmentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AssignmentQueryServiceImpl implements AssignmentQueryService {
+    private final AssignmentRepository assignmentRepository;
+
+    public AssignmentQueryServiceImpl(AssignmentRepository assignmentRepository) {
+        this.assignmentRepository = assignmentRepository;
+    }
+
+    //QUERIES
+    @Override
+    public Optional<Assignment> handle(GetAssignmentByIdQuery query){
+        return assignmentRepository.findById(query.assigmentId());
+    }
+
+    @Override
+    public List<Assignment> handle(GetAllAssignmentsQuery query){
+        return assignmentRepository.findAll();
+    }
+
+}
