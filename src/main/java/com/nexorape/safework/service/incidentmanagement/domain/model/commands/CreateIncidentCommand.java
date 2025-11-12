691d8f04a2e055b1b024859da6ed2f1d05a7041b
AmirbarrabajaLeon <amircasan@gmail.com>
2025-11-12 01:04:20 -0500

    feat: add userId and companyId to CreateIncidentCommand for incident creation

--- src/main/java/com/nexorape/safework/service/incidentmanagement/domain/model/commands/CreateIncidentCommand.java
+++ src/main/java/com/nexorape/safework/service/incidentmanagement/domain/model/commands/CreateIncidentCommand.java
@@ -1,6 +1,11 @@
 package com.nexorape.safework.service.incidentmanagement.domain.model.commands;
 
+import com.nexorape.safework.service.iam.domain.model.aggregates.Company;
+import com.nexorape.safework.service.iam.domain.model.aggregates.User;
+
 public record CreateIncidentCommand(
+        Long userId,
+        Long companyId,
         String title,
         String description,
         String location) {
