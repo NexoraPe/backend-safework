31bd6d03b7962e65856f9d9eca2ec97a795d187f
AmirbarrabajaLeon <amircasan@gmail.com>
2025-11-12 01:04:46 -0500

    feat: temporary enhance IncidentCommandServiceImpl to include user and company retrieval for incident creation

--- src/main/java/com/nexorape/safework/service/incidentmanagement/application/internal/commandservices/IncidentCommandServiceImpl.java
+++ src/main/java/com/nexorape/safework/service/incidentmanagement/application/internal/commandservices/IncidentCommandServiceImpl.java
@@ -1,5 +1,7 @@
 package com.nexorape.safework.service.incidentmanagement.application.internal.commandservices;
 
+import com.nexorape.safework.service.iam.infrastructure.persistence.jpa.repositories.CompanyRepository;
+import com.nexorape.safework.service.iam.infrastructure.persistence.jpa.repositories.UserRepository;
 import com.nexorape.safework.service.incidentmanagement.domain.model.aggregates.Incident;
 import com.nexorape.safework.service.incidentmanagement.domain.model.commands.CreateIncidentCommand;
 import com.nexorape.safework.service.incidentmanagement.domain.services.IncidentCommandService;
@@ -13,13 +15,24 @@ public class IncidentCommandServiceImpl implements IncidentCommandService {
 
     private final IncidentRepository incidentRepository;
 
-    public IncidentCommandServiceImpl(IncidentRepository incidentRepository) {
+    private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
+
+    public IncidentCommandServiceImpl(IncidentRepository incidentRepository,  UserRepository userRepository, CompanyRepository companyRepository) {
         this.incidentRepository = incidentRepository;
+        this.userRepository = userRepository;
+        this.companyRepository = companyRepository;
     }
 
     @Override
     public Optional<Incident> handle(CreateIncidentCommand command){
-        var incident = new Incident(command);
+
+        // SOLUCION RAPIDA, MAS ADELANTE YA SEGUIMOS DDD AL 100% XD
+
+        var user = userRepository.findById(command.userId()).orElseThrow(() -> new RuntimeException("User id not found"));;
+        var company = companyRepository.findById(command.companyId()).orElseThrow(() -> new RuntimeException("Company id not found"));;
+
+        var incident = new Incident(user, company, command);
         incidentRepository.save(incident);
         return Optional.of(incident);
     }
